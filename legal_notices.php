5a3606ca45ea08d61a7ae7e870d6b66fb35bb75c
Jason Hejna <jason.hejna@gmail.com>
2012-11-09 13:20:30 -0500

    added favicon

--- legal_notices.php
+++ legal_notices.php
@@ -7,8 +7,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="author" content="Jason Hejna">
-<!--   <link rel="shortcut icon" href="images/gauge.ico">
-  <link rel="icon" href="images/gauge.ico"> -->
+<link rel="shortcut icon" href="images/gauge.ico">
+  <link rel="icon" href="images/gauge.ico">
 <title>Jasso Lopez PLLC - Corporate, Securities and M&A Law</title>
 
 <link rel="stylesheet" href="style.css" />
